c94c43bf7516c4e4791e7d1099a3810ee185b74f
Jonathan Molero <molerojj@gmail.com>
2025-11-05 15:22:57 -0400

    fix center revistas

--- src/pages/Transmision.jsx
+++ src/pages/Transmision.jsx
@@ -140,7 +140,7 @@ const Transmision = () => {
             ? formatFechaJornada(fechaJornada)
             : 'Cargando...'}
         </p>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 justify-items-center">
+        <div className="flex flex-wrap justify-center gap-8">
           {revistas.map((file, index) => (
             <div
               key={index}
