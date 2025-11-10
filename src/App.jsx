4722707b21e4486a2cb099cf7845e1825309a037
Jonathan Molero <molerojj@gmail.com>
2025-11-10 15:36:20 -0400

    transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import UploadPage from './components/Upload';
 import Home from './pages/Home';
 import Transmision from './pages/Transmision';
+import AnimatedPage from './components/AnimatedPage';
 
 const App = () => {
   return (
@@ -18,23 +26,42 @@ const Layout = () => {
   const location = useLocation();
   const path = location.pathname;
 
-  // Mostrar Navbar solo en la ruta raíz
-  const showNavbar = path === '/';
+  const routeConfig = {
+    '/': { navbar: true, footer: true },
+    '/transmision': { navbar: false, footer: true },
+    '/upload': { navbar: false, footer: false },
+  };
 
-  // Mostrar Footer en / y /transmision
-  const showFooter = path === '/' || path === '/transmision';
+  const config = routeConfig[path] || { navbar: false, footer: false };
 
   return (
     <>
-      {showNavbar && <Navbar />}
+      {config.navbar && <Navbar />}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/transmision" element={<Transmision />} />
-        <Route path="/upload" element={<UploadPage />} />
-      </Routes>
+      {/* Rutas animadas */}
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <AnimatedPage>
+                <Home />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/transmision"
+            element={
+              <AnimatedPage>
+                <Transmision />
+              </AnimatedPage>
+            }
+          />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </AnimatePresence>
 
-      {showFooter && <Footer />}
+      {config.footer && <Footer />}
     </>
   );
 };
